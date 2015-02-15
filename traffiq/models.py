08af2a4362d5441128539a27af76580b6ed2d10c
bayo <bayokrapht@gmail.com>
2015-02-16 00:18:05 +0100

    added last location data

--- traffiq/models.py
+++ traffiq/models.py
@@ -5,6 +5,9 @@ from datetime import datetime
 class TrafficReport(models.Model):
     latitude = models.CharField(max_length=20)
     longitude = models.CharField(max_length=50)
+    last_latitude = models.CharField(max_length=20, blank=True)
+    last_longitude = models.CharField(max_length=50, blank=True)
+    speed = models.CharField(max_length=50, blank=True)
     response = models.CharField(max_length=10)
     when = models.DateTimeField(default=datetime.now)
 
