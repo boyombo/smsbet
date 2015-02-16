0ca4df72b72923d2e2d3d229868a8733645294a7
bayo <bayokrapht@gmail.com>
2015-02-17 00:53:00 +0100

    testing

--- traffiq/views.py
+++ traffiq/views.py
@@ -43,7 +43,7 @@ def map(request):
 
 
 def get_markers(request):
-    six_hrs_ago = timezone.now() - timezone.timedelta(hours=6)
+    six_hrs_ago = timezone.now() - timezone.timedelta(hours=24)
     markers = []
     for rep in TrafficReport.objects.filter(when__gte=six_hrs_ago):
         try:
