d16f323a981832484267d75c3f60617145b09680
bayo <bayokrapht@gmail.com>
2015-08-13 20:10:38 +0100

    changed markers time limit to 1 week

--- traffiq/views.py
+++ traffiq/views.py
@@ -43,7 +43,8 @@ def map(request):
 
 
 def get_markers(request):
-    six_hrs_ago = timezone.now() - timezone.timedelta(hours=2)
+    #six_hrs_ago = timezone.now() - timezone.timedelta(hours=2)
+    six_hrs_ago = timezone.now() - timezone.timedelta(hours=168)
     markers = []
     for rep in TrafficReport.objects.filter(when__gte=six_hrs_ago):
         try:
